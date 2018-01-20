4a0002e73ffe66042d7ca947a7b9dd6eaf03c8a5
김병준 <qudwns0524@gmail.com>
2018-01-21 00:04:53 +0900

    Update 1303.c

--- 1303.c
+++ 1303.c
@@ -10,13 +10,13 @@
  
 #include <stdio.h>
 int main(void) {
-    int h, w;
-    scanf("%d %d", &h, &w);
+    int h, w; //height, width
+    scanf("%d %d", &h, &w); //입력을 받는다.
     for (int i = 0; i < h; i++) {
-        for (int j = 1; j <= w; j++) {
+        for (int j = 1; j <= w; j++) { //for문 두개를 중첩시켜 2차원 배열 형태의 숫자를 출력한다.
             printf("%d ", i * w + j);
         }
-        printf("\n", i * w + h);
+        printf("\n", i * w + h); //한 줄 출력이 끝날 때 마다 줄바꿈을 해 준다.
     }
     return 0;
 }
