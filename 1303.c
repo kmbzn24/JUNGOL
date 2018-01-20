e9e3afa30130e611e5d3aac6cad85d6d9b2b9124
김병준 <qudwns0524@gmail.com>
2018-01-20 23:50:07 +0900

    Create 1303.c

---
+++ 1303.c
@@ -0,0 +1,21 @@
+/**************************************************************
+    Problem: 1303
+    User: qudwns0524
+    Language: C
+    Result: Success
+    Time:1 ms
+    Memory:1092 kb
+****************************************************************/
+
+#include <stdio.h>
+int main(void) {
+    int h, w;
+    scanf("%d %d", &h, &w);
+    for (int i = 0; i < h; i++) {
+        for (int j = 1; j <= w; j++) {
+            printf("%d ", i * w + j);
+        }
+        printf("\n", i * w + h);
+    }
+    return 0;
+}
